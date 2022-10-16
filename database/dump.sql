4d235cbe372f5569e9fd2dcd3fc98aaafa853ef4
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2022-10-16 19:48:16 +0200

    post

--- database/dump.sql
+++ database/dump.sql
@@ -5,3 +5,9 @@ create table `utilisateur` (
     mail VARCHAR(50) NULL    
    
 ); 
+create table `post` (     
+    id_post int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,    
+    id_utilisateur INT REFERENCES utilisateur (id_utilisateur),
+    text text NOT NULL,     
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id_utilisateur)
+); 
--- src/blog.php
+++ src/blog.php
@@ -1,5 +1,114 @@
 <?php
     session_start();
     require('db.php');
-echo $_SESSION['name'] ;
+    if(isset($_POST['envoi'])){
+        if (!isset($_POST['post_blog'])){
+            echo 'vous avez pas remplis tous les champs';
+        }else{
+            // pour que l'utilisateur de rentre pas de l'html
+            $post_blog = htmlspecialchars($_POST['post_blog']);
+            $id_utilisateur = $_SESSION['id_utilisateur'];
+             //partie insertion
+            //preparation de la requete
+            $requete_blog = $bdd->prepare("INSERT INTO post(id_utilisateur,text) VALUES ('".$id_utilisateur."','".$post_blog."');");
+            //execution de la requete
+            $requete_blog->execute();
+            //si il y a pas d'information ou manque des information
+            header('Location: blog.php');
+            
+            
+            // verifie si le mail est deja dans la base de donnée
+            
+            
+            
+                
+    
+            }
+        }
+    
+    
+    
+    
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="blog.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="container_popup" id="myModal">
+            <div class="modal-content">
+                <div class="container_content">
+                    <div class="title">
+                        
+                        <span class="close">&times;</span>
+                    </div>
+                    <div class="input">
+
+                        <div class="text">Ajouter un poste</div>
+
+                        <form action="blog.php" method="POST">
+                            <div></div>
+                            <input type="text"  name="post_blog" class="post_blog">
+                            <input type="submit"  name="envoi">
+                        </form>
+                    </div>
+                    
+                </div>
+               
+                
+            </div>
+
+        </div>
+        <div class="header">
+            <div class="name"><?php echo $_SESSION['name'];?>
+            </div>
+            <div class="ajouter">
+                <img src="plus.png" alt="" id="myBtn">
+            </div>
+            <img src="deco.png" alt="">
+
+        </div>
+        
+        <div class="container_blog">
+            <div class="all_post">
+
+            </div>
+        </div>
+    </div>
+    <script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+</body>
+</html>
